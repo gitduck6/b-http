f7f4c07a7b94d4636ad7777981111cf175961426
gitduck6 <warmsot19@gmail.com>
2026-01-20 20:51:49 +0400

    Made a config reader

---
+++ src/confhandler.c
@@ -0,0 +1,58 @@
+#include "confhandler.h"
+
+int config_init(Conf * config_pointer)
+{
+    size_t buffer_size = 256;
+    char * conf_buffer = malloc(buffer_size);
+
+    if (conf_buffer == NULL) {
+        perror("Memory allocation issue");
+        return NULL;
+    }
+
+    FILE * fp = fopen(CONF_FILE,"rb");
+    if (fp == NULL)
+    {
+        // IF we couldnt open the file, we put a basic config file in its place
+        config_pointer->port = DEFAULT_PORT;
+        config_pointer->webroot = DEFAULT_WEBROOT;
+        return 1;
+    }
+
+    int c;
+    for (int i = 0;(c = fgetc(fp)) != EOF;i++)
+    {
+        if ((i + 1) >= buffer_size)
+        {
+            buffer_size *= 2;
+            char * temp = realloc(conf_buffer,buffer_size);
+            if (temp == NULL)
+            {
+                perror("Realloc error\n");
+                free(conf_buffer);
+                return -1;
+            }
+            conf_buffer = temp;
+
+        }
+        conf_buffer[i] = (char)c;
+    }
+
+    char *portPtr = strstr(conf_buffer,"PORT");
+
+    if (portPtr)
+    sscanf(portPtr,"PORT=%d;",&(config_pointer->port));
+    else 
+    config_pointer->port = DEFAULT_PORT;
+
+
+    char *webrootPtr = strstr(conf_buffer,"WEBROOT");
+
+    if (webrootPtr)
+    sscanf(webrootPtr,"WEBROOT=%s;",&(config_pointer->webroot)); 
+    else
+    config_pointer->webroot = DEFAULT_WEBROOT;
+
+    return 0;
+
+}
---
+++ src/confhandler.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define CONF_FILE "server.conf"
+
+#define DEFAULT_PORT 8080
+#define DEFAULT_WEBROOT "www"
+
+
+typedef struct conf
+{
+    int port;
+    char * webroot;
+}
+Conf;
